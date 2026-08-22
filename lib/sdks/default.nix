7e47b488b696fb0c73e137f19aac7303b6931234
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-08-21 20:38:52 -0500

    Yarn classic support (#14)

--- lib/sdks/default.nix
+++ lib/sdks/default.nix
@@ -1,6 +1,7 @@
 { callPackage }:
 {
   nodejs = callPackage ./nodejs.nix { };
+  yarnNodejs = callPackage ./yarn-nodejs.nix { };
   go = callPackage ./go.nix { };
   dotnet = callPackage ./dotnet.nix { };
 }
---
+++ lib/sdks/yarn-nodejs.nix
@@ -0,0 +1,60 @@
+{
+  lib,
+  stdenv,
+  fetchYarnDeps,
+  yarnConfigHook,
+  yarnBuildHook,
+  yarnInstallHook,
+}:
+# Companion to nodejs.nix for hand-written, non-codegen'd nodejs Pulumi
+# packages (e.g. a plain component-resource library, not generated from a
+# schema) that use yarn classic instead of npm - nodejs.nix's `lockFile`
+# convention assumes a `package-lock.json`, which yarn-classic projects
+# don't have (and regenerating one from `yarn.lock` corrupts its
+# `resolved` URLs). This produces the same `$out/lib/node_modules/<pkgName>`
+# output shape as nodejs.nix, so callers (and `with-sdks.nix`) don't need
+# to care which one was used.
+{
+  meta,
+  pname,
+  src,
+  version,
+  yarnLockFile,
+  yarnDepsHash,
+  ...
+}@args:
+stdenv.mkDerivation (
+  {
+    inherit
+      meta
+      pname
+      src
+      version
+      ;
+
+    sourceRoot = "${src.name}/sdk/nodejs";
+
+    nativeBuildInputs = [
+      yarnConfigHook
+      yarnBuildHook
+      yarnInstallHook
+    ];
+
+    yarnOfflineCache = fetchYarnDeps {
+      yarnLock = yarnLockFile;
+      hash = yarnDepsHash;
+    };
+
+    postPatch = ''
+      cp ${yarnLockFile} yarn.lock
+      sed -i \
+        -e 's/"version": ".*"/"version": "${version}"/' \
+        package.json
+    '';
+  }
+  // lib.optionalAttrs (!(args ? yarnBuildScript)) { dontYarnBuild = true; }
+  // (lib.removeAttrs args [
+    "yarnLockFile"
+    "yarnDepsHash"
+  ])
+)
